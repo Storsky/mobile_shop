b44807a5dbe708fcec4f066fbd6055cb440c2fe1
Storsky <andrkvasn@yandex.ru>
2022-11-28 15:55:33 +0400

    created catalog page

--- mobile_shop/phones/urls.py
+++ mobile_shop/phones/urls.py
@@ -1,2 +1,7 @@
 from django.urls import path
+from .views import *
 
+urlpatterns = [
+    path('', index),
+    path('catalog', show_products, name = 'catalog')
+]
--- mobile_shop/phones/views.py
+++ mobile_shop/phones/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Model_id
+def index(request):
+    return redirect('catalog')
 
 # Create your views here.
+def show_products(request):
+    template = 'phones.html'
+    context = {'phones': Model_id.objects.all()}
+    return render(request, template, context)
